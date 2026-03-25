22fb84722d6f897f8943adf3425b9c353aa24fc4
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-03-25 12:06:01 +0000

    did it to the wrong one put use this as an exapmle

--- test_score.py
+++ test_score.py
@@ -1,16 +1,19 @@
 import pytest
 from score import Score
 
+
 def test_add_points():
     s = Score()
     s.add_points(10)
     assert s.points == 10
+
 
 def test_reset():
     s = Score()
     s.add_points(5)
     s.reset()
     assert s.points == 0
+
 
 def test_negative_raises():
     s = Score()
